98e556bfe0e512703dfcc5c2ba323e8f2744eff8
Ahmed <gfg59204@gmail.com>
2023-12-16 00:19:14 +0400

    remove tailwind

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,65 +2,26 @@ import prismadb from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
-export default  async function SetupLayout({children}:{children:React.ReactNode}) {
+export default async function SetupLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { userId } = auth();
 
+  if (!userId) {
+    redirect("/sign-in");
+  }
 
-const {userId}=auth();
+  const store = await prismadb.store.findFirst({
+    where: {
+      userId,
+    },
+  });
 
+  if (store) {
+    redirect(`/${store.id}`);
+  }
 
-
-if(!userId){
-
-
-
-redirect('/sign-in')
-
-
-
-}
-
-
-const store = await prismadb.store.findFirst({
-where:{
-
-userId
-
-}
-
-
-
-})
-
-
-
-if (store) {
-    
-redirect(`/${store.id}`)
-
-
-}
-
-
-if (!store) {
-  
-    redirect('/')   
-   
-   }
-
-return (
-
-<>
-
-
-
-{children}
-
-</>
-
-
-)
-
-
-
-    
+  return <>{children}</>;
 }
